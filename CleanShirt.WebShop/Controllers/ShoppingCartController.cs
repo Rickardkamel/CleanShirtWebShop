90ea531a20048f84be5fbb5fa7171df6515630b1
Rickardkamel <rickardkamel@gmail.com>
2016-11-15 22:50:21 +0100

    created controllers for different views.

--- CleanShirt.WebShop/Controllers/ShoppingCartController.cs
+++ CleanShirt.WebShop/Controllers/ShoppingCartController.cs
@@ -6,8 +6,9 @@ using System.Web.Mvc;
 
 namespace CleanShirt.WebShop.Controllers
 {
-    public class HomeController : Controller
+    public class ShoppingCartController : Controller
     {
+        // GET: ShoppingCart
         public ActionResult Index()
         {
             return View();
