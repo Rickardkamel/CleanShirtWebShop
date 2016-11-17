22e8929dab141bd4091a2cec2c99903b95e22ead
rickardkamel <rickardkamel@gmail.com>
2016-11-17 17:02:02 +0100

    Created product-viewmodel.

---
+++ CleanShirt.WebShop/ViewModels/ProductViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CleanShirt.WebShop.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int QuantityInStorage { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
