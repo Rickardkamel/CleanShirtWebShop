bc43b7e4c850de34e7fb901d59f158dcbbbc9830
rickardkamel <rickardkamel@gmail.com>
2016-11-17 17:01:07 +0100

    Added controller & handler for Product.

---
+++ CleanShirt.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CleanShirt.WebApi.Contracts;
+using CleanShirt.WebApi.DataService;
+using CleanShirt.WebApi.Handlers;
+
+namespace CleanShirt.WebApi.Controllers
+{
+    public class ProductController : ApiController
+    {
+        private readonly ProductHandler _productHandler;
+
+        public ProductController()
+        {
+            _productHandler = new ProductHandler(new DataContext());
+        }
+
+        public IEnumerable<ProductContract> Get()
+        {
+            return _productHandler.Get();
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            var product = _productHandler.Get(id);
+            return product == null ? (IHttpActionResult)NotFound() : Ok(product);
+        }
+
+        // NOT BEING USED ATM TODO: CHECK IF WE NEED POST/DELETE
+        public IHttpActionResult Post(ProductContract productContract)
+        {
+            _productHandler.Post(productContract);
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            _productHandler.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ CleanShirt.WebApi/Handlers/ProductHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using CleanShirt.WebApi.Contracts;
+using CleanShirt.WebApi.DataService;
+using CleanShirt.WebApi.DataService.UnitOfWork;
+using CleanShirt.WebApi.Mappers;
+
+namespace CleanShirt.WebApi.Handlers
+{
+    public class ProductHandler
+    {
+        private readonly UnitOfWork _uow;
+        public ProductHandler(object dataContext)
+        {
+            _uow = new UnitOfWork((DataContext) dataContext);
+        }
+
+        public List<ProductContract> Get()
+        {
+            return _uow.ProductRepository.GetAll().ToContracts();
+        }
+
+        public ProductContract Get(int id)
+        {
+            return _uow.ProductRepository.Get(id).ToContract();
+        }
+
+        public void Post(ProductContract productContract)
+        {
+            _uow.ProductRepository.CreateOrUpdate(productContract.ToEntity());
+        }
+
+        public void Delete(int id)
+        {
+            _uow.ProductRepository.Delete(id);
+        }
+
+    }
+}
