6357c7c678c76199aedb2055175c815b9634521b
rickardkamel <rickardkamel@gmail.com>
2016-11-17 17:01:48 +0100

    added test to get products.

--- CleanShirt.WebShop/Controllers/HomeController.cs
+++ CleanShirt.WebShop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CleanShirt.WebApi.Controllers;
 
 namespace CleanShirt.WebShop.Controllers
 {
@@ -10,7 +11,11 @@ namespace CleanShirt.WebShop.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var x = new ProductController();
+
+            var c = x.Get();
+
+            return View(c);
         }
     }
 }
