fc51d4e23fc18490d697e96e78ff94acb98caf8f
rickardkamel <rickardkamel@gmail.com>
2016-11-18 16:59:58 +0100

    added viewmodels for the WebShop.

---
+++ CleanShirt.WebShop/ViewModels/ShoppingCartItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CleanShirt.WebShop.ViewModels
+{
+    public class ShoppingCartItemViewModel
+    {
+        public ProductViewModel Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ CleanShirt.WebShop/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CleanShirt.WebShop.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<ShoppingCartItemViewModel> ShoppingCartItem { get; set; }
+    }
+}
