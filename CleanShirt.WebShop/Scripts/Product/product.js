47f825bae907077e8e15d0600f894d9bfa35147c
Rickard Kamel <rickardkamel@gmail.com>
2016-11-19 19:47:52 +0100

    removed comments.

--- CleanShirt.WebShop/Scripts/Product/product.js
+++ CleanShirt.WebShop/Scripts/Product/product.js
@@ -1,44 +1,13 @@
 ﻿
 $(document).ready(function () {
-    //var dataToSend;
-    //test();
-    //function test() {
-    //    $.get("http://localhost:53365/api/product", function (data, status) {
-    //        sendData(data);
-    //    });
-    //    function sendData(data) {
-    //        $.ajax({
-    //            url: '/Home/Index',
-    //            type: 'POST',
-    //            data: JSON.stringify(data),
-    //            dataType: 'json',
-    //            contentType: 'application/json; charset=utf-8',
-    //        });
-    //    }
-    //}
-
-
-
 });
 
-
 function addToCart(value) {
-
     $.get("http://localhost:53365/api/product/" + value,
         function (data, status) {
-
             $.post('Home/AddToCart', data, function () {
-                // Call the custom function here
                 updateCartTotal(data.Price);
             });
-
-            //$.ajax({
-            //    url: '/Home/AddToCart',
-            //    type: 'POST',
-            //    data: JSON.stringify(data),
-            //    dataType: 'json',
-            //    contentType: 'application/json; charset=utf-8'
-            //});
         });
 }
 
