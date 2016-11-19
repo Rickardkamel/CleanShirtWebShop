79b2770f927f3d95f37f9e66b15cf90d5ff2a914
Rickard Kamel <rickardkamel@gmail.com>
2016-11-20 00:40:39 +0100

    added viewModel for customer.

---
+++ CleanShirt.WebShop/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanShirt.WebShop.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+    }
+}
