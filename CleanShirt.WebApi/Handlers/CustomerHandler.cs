73bd0dd5a6467a1f651c55149178a1be69565556
Rickardkamel <rickardkamel@gmail.com>
2016-11-21 15:46:13 +0100

    added handler for customer/order.

---
+++ CleanShirt.WebApi/Handlers/CustomerHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CleanShirt.WebApi.DataService;
+using CleanShirt.WebApi.DataService.UnitOfWork;
+using CleanShirt.WebApi.Mappers;
+using Contracts;
+
+namespace CleanShirt.WebApi.Handlers
+{
+    public class CustomerHandler
+    {
+        private readonly UnitOfWork _uow;
+        public CustomerHandler(object dataContext)
+        {
+            _uow = new UnitOfWork((DataContext)dataContext);
+        }
+
+        public List<CustomerContract> Get()
+        {
+            return _uow.CustomerRepository.GetAll().ToContracts();
+        }
+
+        public CustomerContract Get(int id)
+        {
+            return _uow.CustomerRepository.Get(id).ToContract();
+        }
+
+        public void Post(CustomerContract customerContract)
+        {
+            _uow.CustomerRepository.CreateOrUpdate(customerContract.ToEntity());
+        }
+
+        public void Delete(int id)
+        {
+            _uow.CustomerRepository.Delete(id);
+        }
+    }
+}
---
+++ CleanShirt.WebApi/Handlers/OrderHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CleanShirt.WebApi.DataService;
+using CleanShirt.WebApi.DataService.UnitOfWork;
+using CleanShirt.WebApi.Mappers;
+using Contracts;
+
+namespace CleanShirt.WebApi.Handlers
+{
+    public class OrderHandler
+    {
+        private readonly UnitOfWork _uow;
+        public OrderHandler(object dataContext)
+        {
+            _uow = new UnitOfWork((DataContext)dataContext);
+        }
+
+        public List<OrderContract> Get()
+        {
+            return _uow.OrderRepository.GetAll().ToContracts();
+        }
+
+        public OrderContract Get(int id)
+        {
+            return _uow.OrderRepository.Get(id).ToContract();
+        }
+
+        public void Post(OrderContract orderContract)
+        {
+            // set date to today, TODO: set null on billeddate & sentdate
+            orderContract.OrderedDate = DateTime.Now;
+            orderContract.BilledDate = DateTime.Now;
+            orderContract.SentDate = DateTime.Now;
+
+            // Create the costumer
+            var customer = _uow.CustomerRepository.CreateOrUpdate(orderContract.Customer.ToEntity());
+            orderContract.CustomerId = customer.Id;
+
+            // remove quantity from product
+
+            foreach (var item in orderContract.OrderLines)
+            {
+                var productFromDb = _uow.ProductRepository.Get(item.ProductId);
+
+                productFromDb.QuantityInStorage = productFromDb.QuantityInStorage - item.Quantity;
+                _uow.ProductRepository.CreateOrUpdate(productFromDb);
+            }
+
+            _uow.OrderRepository.CreateOrUpdate(orderContract.ToEntity());
+        }
+
+        public void Delete(int id)
+        {
+            _uow.OrderRepository.Delete(id);
+        }
+    }
+}
