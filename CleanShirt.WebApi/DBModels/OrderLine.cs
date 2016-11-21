441ccdb0c25b302b4a66b59da8d55aa2d4c7564c
Rickardkamel <rickardkamel@gmail.com>
2016-11-21 15:46:03 +0100

    Added productId.

--- CleanShirt.WebApi/DBModels/OrderLine.cs
+++ CleanShirt.WebApi/DBModels/OrderLine.cs
@@ -8,6 +8,7 @@ namespace CleanShirt.WebApi.DBModels
     public class OrderLine
     {
         public int Id { get; set; }
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public int PricePerProduct { get; set; }
