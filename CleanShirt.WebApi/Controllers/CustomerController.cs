8f048c50b01220cba1f1331d9d6e2011cbdbd193
Rickardkamel <rickardkamel@gmail.com>
2016-11-21 15:45:10 +0100

    Added controllers.

---
+++ CleanShirt.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using CleanShirt.WebApi.DataService;
+using CleanShirt.WebApi.Handlers;
+using Contracts;
+
+namespace CleanShirt.WebApi.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private readonly CustomerHandler _customerHandler;
+
+        public CustomerController()
+        {
+            _customerHandler = new CustomerHandler(new DataContext());
+        }
+
+        public IEnumerable<CustomerContract> Get()
+        {
+            return _customerHandler.Get();
+        }
+
+        public CustomerContract Get(int id)
+        {
+            var customer = _customerHandler.Get(id);
+            return customer;
+        }
+
+        public IHttpActionResult Post(CustomerContract customerContract)
+        {
+            _customerHandler.Post(customerContract);
+            return Ok();
+        }
+
+        // TODO: CHECK IF WE NEED DELETE
+        public IHttpActionResult Delete(int id)
+        {
+            _customerHandler.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ CleanShirt.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using CleanShirt.WebApi.DataService;
+using CleanShirt.WebApi.Handlers;
+using Contracts;
+
+namespace CleanShirt.WebApi.Controllers
+{
+    public class OrderController : ApiController
+    {
+        private readonly OrderHandler _orderHandler;
+
+        public OrderController()
+        {
+            _orderHandler = new OrderHandler(new DataContext());
+        }
+
+        public IEnumerable<OrderContract> Get()
+        {
+            return _orderHandler.Get();
+        }
+
+        public OrderContract Get(int id)
+        {
+            var order = _orderHandler.Get(id);
+            return order;
+        }
+
+        public IHttpActionResult Post(OrderContract orderContract)
+        {
+            _orderHandler.Post(orderContract);
+            return Ok();
+        }
+
+        // TODO: CHECK IF WE NEED DELETE
+        public IHttpActionResult Delete(int id)
+        {
+            _orderHandler.Delete(id);
+            return Ok();
+        }
+    }
+}
