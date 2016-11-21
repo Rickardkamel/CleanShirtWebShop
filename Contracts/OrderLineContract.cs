12434187907e1da5a6b0fc560876c6a01653aeba
Rickardkamel <rickardkamel@gmail.com>
2016-11-21 15:41:39 +0100

    added productID.

--- Contracts/OrderLineContract.cs
+++ Contracts/OrderLineContract.cs
@@ -3,6 +3,7 @@
     public class OrderLineContract
     {
         public int Id { get; set; }
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public int PricePerProduct { get; set; }
