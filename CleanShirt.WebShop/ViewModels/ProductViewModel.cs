34985f2f23e2ad224fe5021a832e52fde9e9508e
Rickardkamel <rickardkamel@gmail.com>
2016-11-21 15:43:34 +0100

    ProductId.

--- CleanShirt.WebShop/ViewModels/ProductViewModel.cs
+++ CleanShirt.WebShop/ViewModels/ProductViewModel.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Linq;
 using System.Web;
+using Contracts;
 
 namespace CleanShirt.WebShop.ViewModels
 {
-    public class ProductViewModel
+    public class ProductViewModel: ProductContract
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
-        public int QuantityInStorage { get; set; }
-        public string ImageUrl { get; set; }
     }
 }
