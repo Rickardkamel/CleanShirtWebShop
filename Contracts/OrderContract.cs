5628d9b27dd8ca3d043a15ef4d03d44c28a42987
ricka <ricka@DESKTOP-8L2QTEV>
2016-11-22 14:29:20 +0100

    added nullable on billedDate & sentDate.

--- Contracts/OrderContract.cs
+++ Contracts/OrderContract.cs
@@ -11,8 +11,8 @@ namespace Contracts
         public DateTime OrderedDate { get; set; }
         public bool Billed { get; set; }
         public bool Sent { get; set; }
-        public DateTime BilledDate { get; set; }
-        public DateTime SentDate { get; set; }
+        public DateTime? BilledDate { get; set; }
+        public DateTime? SentDate { get; set; }
 
         public virtual CustomerContract Customer { get; set; }
         public virtual IList<OrderLineContract> OrderLines { get; set; }
