ff19638966a78a86770ea89e9a831eb78e6f6587
rickardkamel <ricka@DESKTOP-8L2QTEV>
2016-11-29 13:29:49 +0100

    added logic for queue posts.

--- CleanShirt.BillingSystem/Scripts/Billing/billing-list.js
+++ CleanShirt.BillingSystem/Scripts/Billing/billing-list.js
@@ -1,26 +1,70 @@
 ﻿$(document).ready(function () {
-    $.get("http://localhost:53365/api/order/",
-        function (data, status) {
-            $.ajax({
-                url: 'Home/BillingList',
-                traditional: true,
-                data: JSON.stringify(data),
-                contentType: "application/json; charset=utf-8",
-                type: 'POST',
-                success: function (data) {
-                    $("#partialView").html(data);
-                }
-            });
-        });
+
 });
+getAllOrders();
+getNewOrder();
+
+
+function getAllOrders() {
+    $.get("http://localhost:53365/api/order/",
+       function (data, status) {
+           $.ajax({
+               url: 'Home/BillingList',
+               traditional: true,
+               data: JSON.stringify(data),
+               contentType: "application/json; charset=utf-8",
+               type: 'POST',
+               success: function (data) {
+                   $("#partialView").append(data);
+               }
+           });
+       });
+}
+
+function getNewOrder() {
+    $.get("http://localhost:53365/api/order/GetNewOrders/" + "NewOrder",
+        function (messageResponse) {
+            if (messageResponse === "") {
+                getNewOrder();
+            } else {
+                $.get("http://localhost:53365/api/order/" + messageResponse,
+                function (order) {
+                    $.ajax({
+                        url: 'Home/BillingList',
+                        traditional: true,
+                        data: JSON.stringify([order]),
+                        contentType: "application/json; charset=utf-8",
+                        type: 'POST',
+                        success: function (orderToAppend) {
+                            $("#partialView").append(orderToAppend);
+                            getNewOrder();
+                        }
+                    });
+                });
+            }
+        });
+}
+
+function showDetails(itemId) {
+    $("#productlist-" + itemId).toggle(1000);
+};
 
 function invoiceOrder(order) {
     order.BilledDate = new Date().toUTCString();
     order.OrderedDate = parseDate(order.OrderedDate);
     order.Billed = true;
-    $.post("http://localhost:53365/api/order", order)
+    $.post("http://localhost:53365/api/order/" + "InvoiceOrder", order)
     .done(function (response) {
-        console.log("Data returned: " + response);
+        $.ajax({
+            url: 'Home/BillingList',
+            traditional: true,
+            data: JSON.stringify([response]),
+            contentType: "application/json; charset=utf-8",
+            type: 'POST',
+            success: function (orderToAppend) {
+                $("#row-"+order.Id).replaceWith(orderToAppend);
+            }
+        });
     });
 };
 
@@ -38,9 +82,18 @@ function unInvoiceOrder(order) {
     order.BilledDate = new Date().toUTCString();
     order.OrderedDate = parseDate(order.OrderedDate);
     order.Billed = false;
-    $.post("http://localhost:53365/api/order", order)
+    $.post("http://localhost:53365/api/order/" + "InvoiceOrder", order)
     .done(function (response) {
-        console.log("Data returned: " + response);
+        $.ajax({
+            url: 'Home/BillingList',
+            traditional: true,
+            data: JSON.stringify([response]),
+            contentType: "application/json; charset=utf-8",
+            type: 'POST',
+            success: function (orderToAppend) {
+                $("#row-" + order.Id).replaceWith(orderToAppend);
+            }
+        });
     });
 };
 
