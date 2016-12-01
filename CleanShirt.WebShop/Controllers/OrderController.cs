306bd834332cdf6c0640ba4ff2cffcd09bdedb9e
rickardkamel <ricka@DESKTOP-8L2QTEV>
2016-12-01 10:30:26 +0100

    Added OrderStatus & Code for it. Did some modifications to Layout & Scripts.

---
+++ CleanShirt.WebShop/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CleanShirt.WebShop.ViewModels;
+
+namespace CleanShirt.WebShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult OrderStatusList(List<OrderViewModel> orders)
+        {
+            return PartialView(orders);
+        }
+    }
+}
---
+++ CleanShirt.WebShop/Scripts/Order/orderstatus.js
@@ -0,0 +1,22 @@
+﻿
+getAllOrders();
+
+function getAllOrders() {
+    $.get("http://localhost:53365/api/order",
+       function (data, status) {
+           $.ajax({
+               url: 'Order/OrderStatusList',
+               traditional: true,
+               data: JSON.stringify(data),
+               contentType: "application/json; charset=utf-8",
+               type: 'POST',
+               success: function (data) {
+                   $("#orderPartialView").html(data);
+               }
+           });
+       });
+}
+
+function showDetails(itemId) {
+    $("#productlist-" + itemId).toggle(1000);
+};
