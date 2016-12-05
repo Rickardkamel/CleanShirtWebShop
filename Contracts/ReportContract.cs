54f3ef3c01082d7eb26cde8b3dee80ca92df3bb6
rickardkamel <ricka@DESKTOP-8L2QTEV>
2016-12-05 13:23:47 +0100

    added contracts for report.

---
+++ Contracts/ReportContract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class ReportContract
+    {
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Contracts/ReportDatesContact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public class ReportDatesContact
+    {
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+}
